ea6571f740dc3c9e8af5c2535ecfa72a89815353
NangMyatHsu <nangmyathsu@ucstgi.edu.mm>
2025-07-14 17:28:03 +0630

    0.04_init_mqtt_conn_with_api

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ import 'package:simple/pages/robot_price_calculator.dart';
 import 'package:simple/pages/mqtt_client_page.dart';
 import 'package:simple/widgets/mqtt_api_connection_provider.dart';
 import 'package:simple/pages/ai_model_predict_ui.dart';
-import 'package:simple/states/ai_model_predict_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,9 +26,8 @@ class _MyAppState extends State<MyApp> {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => DatabaseProvider()),
-        ChangeNotifierProvider(create: (context) => APIConnectionProvider()),
         ChangeNotifierProvider(
-          create: (context) => CameraProvider(),
+          create: (context) => APIConnectionProvider(),
         ), // For MQTT API connection
         // Provider တစ်ခုထည့်ချင်ရင် ဒီမှာထည့်ပါ။
       ],
@@ -57,7 +55,7 @@ class _MainPageState extends State<MainPage> {
     RobotPriceCalculatorPage(),
     DatabaseConnection(),
     MqttClientPage(),
-    CameraScreen(), //For AI model prediction
+    CameraScreen(), // AI Model Prediction page
   ];
   // Page တစ်ခုထည့်ချင်ရင် ဒီမှာထည့်ပါ။
   void _onDrawerItemSelected(int index) {
